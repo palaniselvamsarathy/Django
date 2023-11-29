697598efae30d6ff3cc7b1f4dc2d2dc823a945c9
palaniselvamsarathy <palaniselvamsarathy@gmail.com>
2023-11-29 14:27:37 +0530

    test

--- FirstProject/FirstProject/urls.py
+++ FirstProject/FirstProject/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from UserApp import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("index", views.getindexpage),
+    path("service", views.getservicepage),
+    path("about", views.getaboutpage),
+    path("login", views.getloginpage),
 ]
--- FirstProject/UserApp/views.py
+++ FirstProject/UserApp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def getindexpage(request):
+    msg = '<h2> Index Page</h2>'
+    return HttpResponse(msg)
+
+def getaboutpage(request):
+    return HttpResponse('About Page')
+def getservicepage(request):
+    return HttpResponse('Service Page')
+def getloginpage(request):
+    return HttpResponse('Login Page')
