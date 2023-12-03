104e0c9ad9cbd2edb90bc0394b85c6775a047fbf
palaniselvamsarathy <palaniselvamsarathy@gmail.com>
2023-12-03 19:56:15 +0530

    okie

--- FirstProject/UserApp/views.py
+++ FirstProject/UserApp/views.py
@@ -1,10 +1,3 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
 
-def getindex(request):
-    return render(request,'index.html')
-def getabout(request):
-    return render(request,'about.html')
-def getcontact(request):
-    return render(request,'contact.html')
+# Create your views here.
