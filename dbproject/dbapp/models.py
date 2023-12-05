382a44c6849688e85830b45e59f9eb9ea55752a0
palaniselvamsarathy <palaniselvamsarathy@gmail.com>
2023-12-05 15:39:38 +0530

    okie

---
+++ dbproject/dbapp/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Employee(models.Model):
+    eid = models.IntegerField()
+    ename = models.CharField(max_length=32)
+    esal = models.FloatField()
+    ecity = models.CharField(max_length=32)
