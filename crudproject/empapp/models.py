80af77651abda19e198be065c7ea7d2788e8e9b1
palaniselvamsarathy <palaniselvamsarathy@gmail.com>
2023-12-11 19:52:52 +0530

    last

---
+++ crudproject/empapp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Employee(models.Model):
+    eid = models.IntegerField()
+    ename = models.CharField(max_length=32)
+    esal = models.IntegerField()
+    email = models.EmailField()
+
+    class Meta:
+        db_table = "employee"
---
+++ crudproject/empapp/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect
+from empapp.models import Employee
+from empapp.forms import EmployeeForm
+
+# Create your views here.
+def createview(request):
+    if request.method == "POST":
+        empform = EmployeeForm(request.POST)
+        if empform.is_valid():
+            try:
+                empform.save()
+                return redirect('/read')
+            except:
+                pass
+
+    else:
+        empform = EmployeeForm()
+    
+    return render(request,'create.html',{'empform':empform})
+
+def displayview(request):
+    emp_list = Employee.objects.all()
+    return render(request,'display.html',{'emp_list':emp_list})
+
+def deleteview(request,id):
+    empdata = Employee.objects.get(id=id)
+    empdata.delete()
+    return redirect('/read')
+
+def updateview(request):
+    pass
