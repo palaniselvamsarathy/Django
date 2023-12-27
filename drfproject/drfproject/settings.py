37716bc577eef0b89a0adf50f6a95488547ab958
palaniselvamsarathy <palaniselvamsarathy@gmail.com>
2023-12-27 11:46:43 +0530

    okie

--- drfproject/drfproject/settings.py
+++ drfproject/drfproject/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "drfapp",
+    "rest_framework"
 ]
 
 MIDDLEWARE = [
@@ -75,8 +79,10 @@ WSGI_APPLICATION = "drfproject.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "drf8pm",
+        "USER":"root",
+        "PASSWORD":'SarathY1.'
     }
 }
 
