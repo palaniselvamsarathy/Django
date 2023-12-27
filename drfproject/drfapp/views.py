7b412c3beeaf1170fedad57b2a0dc28207efabb9
palaniselvamsarathy <palaniselvamsarathy@gmail.com>
2023-12-27 12:42:55 +0530

    changes

--- drfproject/drfapp/views.py
+++ drfproject/drfapp/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
-
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from drfapp.models import Employee
+from drfapp.serializers import EmployeeSerializer
 # Create your views here.
+
+'''
+API URL: http://127.0.0.1:8000/
+Method: GET
+Required Fields: None
+ACCESS type: Public
+'''
+
+@api_view(['GET'])
+def home(request):
+    emp_data = Employee.objects.all()
+    emp_serializer = EmployeeSerializer(emp_data, many= True)
+    return Response({'status':200,'message':'Home Page Response','avail':True,'payload':emp_serializer.data})
+
+@api_view(['GET'])
+def emp_data(request):
+    emp_data = Employee.objects.all()
+    emp_serializer = EmployeeSerializer(emp_data, many= True)
+    return Response({'employees':emp_serializer.data})
--- drfproject/drfproject/urls.py
+++ drfproject/drfproject/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from drfapp import views
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('',views.home),
+    path('employees',views.emp_data)
 ]
